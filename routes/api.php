c125353743e8a8fdd3c8317bb5a8c4fc840a0536
Sandeep Kumar Dewangan <sandeepdewangan2012@gmail.com>
2024-08-20 16:08:53 +0530

    Remove Pivot in api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/project/apis', function (Request $request) {
     $project = $request->attributes->get('project');
-    // Hide the specified fields
-    $apiBuilders = $project->apiBuilders->makeHidden(['predefined_conditions', 'created_at', 'updated_at', 'deleted_at', 'created_by', 'updated_by', 'deleted_by']);
+
+    // Hide the specified fields, including the pivot attribute
+    $apiBuilders = $project->apiBuilders->each(function ($apiBuilder) {
+        $apiBuilder->makeHidden(['predefined_conditions', 'created_at', 'updated_at', 'deleted_at', 'created_by', 'updated_by', 'deleted_by']);
+        $apiBuilder->pivot->setHidden(['pivot']);
+    });
+
     return response()->json(['api_list' => $apiBuilders, 'status' => 200]);
 })->name('project.apis');
+
