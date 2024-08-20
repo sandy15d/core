b9696e85298d6a0217a8f2aa13c9b808327cd850
Sandeep Kumar Dewangan <sandeepdewangan2012@gmail.com>
2024-08-20 16:25:09 +0530

    hide some field in project api list

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/project/apis', function (Request $request) {
     $project = $request->attributes->get('project');
-
     // Hide the specified fields
-    $apiBuilders = $project->apiBuilders->makeHidden([
-        'predefined_conditions',
-        'created_at',
-        'updated_at',
-        'deleted_at',
-        'created_by',
-        'updated_by',
-        'deleted_by',
-        'method_name',
-        'model'
+    $apiBuilders = $project->apiBuilders->makeHidden(['predefined_conditions', 'created_at', 'updated_at', 'deleted_at', 'created_by',
+        'updated_by', 'deleted_by', 'method_name', 'model'
     ]);
 
     // Define the key mappings (old_key => new_key)
@@ -25,14 +16,27 @@ Route::get('/project/apis', function (Request $request) {
         // Add more key mappings here if needed
     ];
 
-    // Apply key renaming
-    $apiBuilders = $apiBuilders->map(function ($item) use ($keyMappings) {
+    // Define the desired key order
+    $desiredKeyOrder = ['id', 'api_name', 'api_end_point', 'description', 'parameters'];
+
+    // Apply key renaming and rearrange the order of keys
+    $apiBuilders = $apiBuilders->map(function ($item) use ($keyMappings, $desiredKeyOrder) {
+        // Apply key renaming
         $newItem = [];
         foreach ($item->toArray() as $key => $value) {
-            $newKey = $keyMappings[$key] ?? $key; // Rename key if it exists in the mapping
+            $newKey = $keyMappings[$key] ?? $key;
             $newItem[$newKey] = $value;
         }
-        return $newItem;
+
+        // Rearrange the keys according to the desired order
+        $sortedItem = [];
+        foreach ($desiredKeyOrder as $key) {
+            if (array_key_exists($key, $newItem)) {
+                $sortedItem[$key] = $newItem[$key];
+            }
+        }
+
+        return $sortedItem;
     });
 
     return response()->json(['api_list' => $apiBuilders->values(), 'status' => 200]);
